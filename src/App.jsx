fe6fa2f3142f5eb4eb6d81d40bf49affb29e953d
teslmik <teslmik@gmail.com>
2022-10-26 19:53:19 +0300

    Lesson 2

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import './index.scss';
+
+const Modal = ({ open, setOpen, children }) => (
+  <div className={`overlay animated ${open && 'show'}`}>
+    <div className="modal">
+      <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
+        <title />
+        <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+      </svg>
+      {children/*Если надо передать динамические данные*/}
+    </div>
+  </div>
+);
+
+//  ============ Первый способ. Условный рендер. Не подходит, если нужна анимация модалки ===========
+// function App() {
+//   const [open, setOpen] = React.useState(false);
+
+//   return (
+//     <div className="App">
+//       <button onClick={() => setOpen(true)} className="open-modal-btn">
+//         ✨ Открыть окно
+//       </button>
+//       {open && (
+//         <div className="overlay">
+//           <div className="modal">
+//             <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
+//               <title />
+//               <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+//             </svg>
+//             <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" alt="alt" />
+//           </div>
+//         </div>
+//       )}
+//     </div>
+//   );
+// }
+
+//  ============ Второй способ. Подходит, если нужна анимация модалки ===========
+function App() {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <div className="App">
+      <button onClick={() => setOpen(true)} className="open-modal-btn">
+        ✨ Открыть окно
+      </button>
+      <Modal open={open} setOpen={setOpen}>
+        <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" alt="alt" />
+      </Modal>
+    </div>
+  );
+}
+
+export default App;
